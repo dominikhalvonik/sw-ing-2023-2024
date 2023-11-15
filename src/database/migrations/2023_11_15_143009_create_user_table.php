e8828672ef9c2ad7f5ab83adf4b72979cc30ffd3
Dominik Halvonik <dominik.halvonik@gmail.com>
2023-11-15 16:12:39 +0100

    Add models/migrations/seeders

--- src/database/migrations/2023_11_15_143009_create_user_table.php
+++ src/database/migrations/2023_11_15_143009_create_user_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("first_name", 255);
+            $table->string("last_name", 255);
+            $table->integer("age");
+            $table->string("email", 255);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 };
